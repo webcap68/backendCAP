9692a4b5668345395dcefe23ea891d117d6cd3f0
webcap68 <carlos.piacente@gmail.com>
2023-03-26 08:07:21 -0300

    todo hecho menos proyectos, con build nuevo y con clever

--- src/main/java/com/portfolio/cap/WebConfig.java
+++ src/main/java/com/portfolio/cap/WebConfig.java
@@ -14,8 +14,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
-       // registry.addMapping("https://frontendcap68.web.app");
-        registry.addMapping("http://localhost:4200");
+       registry.addMapping("https://frontendcap68.web.app");
+       // registry.addMapping("http://localhost:4200");
     }
     
 }
